4af58b36059a49381602485c85ae25c1381ffddb
Harley Clair <60947601+harley-dane@users.noreply.github.com>
2025-04-08 13:01:09 +0200

    param

--- add-up-numbers/solution1.js
+++ add-up-numbers/solution1.js
@@ -1,3 +1,9 @@
+/**
+ *  * Given a number, return the sum of all numbers from 1 to that number.
+ * @param {*} num   {number} The number to sum up to.
+ * @returns   {number} The sum of all numbers from 1 to num.
+ */
+
 const addUp = (num) => {
     let total = 0;
     for (let i = 1; i <= num; i++) {
